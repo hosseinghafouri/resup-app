2c2201b5cd1e9ef9c3bcf8b7630adf8a6d2d2952
Hossein <81298314+hosseinghafouri@users.noreply.github.com>
2023-06-01 07:40:11 -0700

    Edit

---
+++ src/components/ResBox.js
@@ -0,0 +1,12 @@
+// import photo from './images/w.png';
+function ResBox() {
+  return (
+    <div className="res_box">
+        <div className="icon">
+        <img  src="./w.png" />
+        </div>
+      <p className="pager_num">5</p>
+    </div>
+  );
+}
+export default ResBox;

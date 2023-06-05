7c7c6a3ff11a68f3a2fd1d2e0fd87b69ea066275
Hossein <81298314+hosseinghafouri@users.noreply.github.com>
2023-06-05 02:12:26 -0700

    dainamic it  components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@
 import ResBox from "./components/ResBox";
 import "./App.css";
 
+let data = {
+  pagers : [[1,'on'],[2,'waiting'],[8,'off']],
+}
 function App() {
   return (
     <div className="App">
-        <ResBox number="5" color="var(--off_color)" />
-        <ResBox number="1" color="var(--turn_color" />
-        <ResBox number="2" color="var(--waiting_color)" />
+      {data.pagers.map((item) => (
+        <ResBox number={item[0]} status={item[1]} />
+      ))}
+        {/* <ResBox number="5" color="var(--waiting_color)" />
+        <ResBox number="1" color="var(--off_color)"/>
+        <ResBox number="2" color="var(--turn_color)" /> */}
     </div>
   );
 }

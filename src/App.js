b970d7dcb5ad5a5c3923387baa664476242ccada
Hossein <81298314+hosseinghafouri@users.noreply.github.com>
2023-07-05 07:25:12 -0700

    Edit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 // import logo from './logo.svg';
 import ResBox from "./components/ResBox";
-import Header from "./components/Header"
+import Header from "./components/Header";
 import "./App.css";
 
 let data = {
-  pagers : [[1,'on','192.168.1.4'],[2,'waiting','172.168.2.7'],[8,'off', '192.128.1.8']],
-}
+  pagers: [
+    [1, "on", "192.168.1.4"],
+    [2, "waiting", "172.168.2.7"],
+    [8, "off", "192.128.1.8"],
+  ],
+};
 function App() {
   return (
     <div>
-    <Header />
-    
-    <div className="App">
-      {data.pagers.map((item) => (
-        <ResBox number={item[0]} status={item[1]} ipURL={item[2]} />
-      ))}
+      <Header />
+
+      <div className="App">
+        {data.pagers.map((item) => (
+          <ResBox number={item[0]} status={item[1]} ipURL={item[2]} />
+        ))}
+
+        <div class="custom-loader"></div>
         {/* <ResBox number="5" color="var(--waiting_color)" />
         <ResBox number="1" color="var(--off_color)"/>
         <ResBox number="2" color="var(--turn_color)" /> */}
-    </div>
+      </div>
     </div>
   );
 }
